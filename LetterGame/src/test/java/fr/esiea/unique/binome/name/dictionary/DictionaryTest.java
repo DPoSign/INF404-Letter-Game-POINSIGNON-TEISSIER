c3a723fcde1341bb823479b3ef59bbeb229aa6c8
DPoSign <daniel.poinsignon@gmail.com>
2017-02-06 22:13:21 +0100

    Starting from scratch

--- LetterGame/src/test/java/fr/esiea/unique/binome/name/dictionary/DictionaryTest.java
+++ LetterGame/src/test/java/fr/esiea/unique/binome/name/dictionary/DictionaryTest.java
@@ -1,9 +1,16 @@
 package fr.esiea.unique.binome.name.dictionary;
 
 import org.junit.Before;
+<<<<<<< HEAD
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+=======
 
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+>>>>>>> ea886a0041089b49bff47fad00a340bf50b0e9b1
 
 /**
  * Unit test sample for Dictionary.
@@ -17,6 +24,10 @@ public class DictionaryTest {
         //TODO
     }
 
+<<<<<<< HEAD
+    @Test
+=======
+>>>>>>> ea886a0041089b49bff47fad00a340bf50b0e9b1
     public void testIsWord() {
         assertTrue(dictionary.isWord("maman"));
         assertFalse(dictionary.isWord("namam"));
